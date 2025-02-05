5ca56dd03d7c6488787a3e529d83013861b36af7
Eduardo Campos <92491094+EduuuCR@users.noreply.github.com>
2025-02-05 14:58:50 -0300

    Create rateLimiter.js
    Limit number of requests per IP

---
+++ fidec-project/backend/src/middlewares/rateLimiter.js
@@ -0,0 +1,6 @@
+const rateLimit = require('express-rate-limit');
+module.exports = rateLimit({
+    windowMs: 15 * 60 * 1000, // 15 minutos
+    max: 100, // 100 requisições por IP
+    message: { message: 'Muitas requisições, tente novamente mais tarde' }
+});
