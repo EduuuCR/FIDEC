b0a2c4c6645f4736e0de5e95d067356fa2ba2164
Eduardo Campos <92491094+EduuuCR@users.noreply.github.com>
2025-02-07 12:16:47 -0300

    Updated the code
    Sending emails

--- fidec-project/backend/src/utils/emailSender.js
+++ fidec-project/backend/src/utils/emailSender.js
@@ -5,3 +5,12 @@ const transporter = nodemailer.createTransport({
     service: mailConfig.service,
     auth: mailConfig.auth
 });
+
+module.exports = async (to, subject, text) => {
+    try {
+        await transporter.sendMail({ from: mailConfig.auth.user, to, subject, text });
+        console.log('E-mail enviado com sucesso!');
+    } catch (err) {
+        console.error('Erro ao enviar e-mail:', err);
+    }
+};
