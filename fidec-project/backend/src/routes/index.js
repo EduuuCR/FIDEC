593b9252f3b58582c5af6fcbf8aeea2bd4189f4e
Eduardo Campos <92491094+EduuuCR@users.noreply.github.com>
2025-02-12 14:01:55 -0300

    Update index.js
    user routes (/users/register, /users/login, etc.) will be available

--- fidec-project/backend/src/routes/index.js
+++ fidec-project/backend/src/routes/index.js
@@ -3,9 +3,11 @@ const router = express.Router();
 
 const contactRoutes = require("./contact");
 const serviceRoutes = require("./services");
+const userRoutes = require("./user"); // Adicionando a nova rota de usuários
 
-router.use("/contact", contactRoutes);
-router.use("/services", serviceRoutes);
+router.use("/contacts", contactRoutes);  // Rotas de contato
+router.use("/services", serviceRoutes);  // Rotas de serviços
+router.use("/users", userRoutes);        // Rotas de autenticação e perfil
 
 module.exports = router;
 
